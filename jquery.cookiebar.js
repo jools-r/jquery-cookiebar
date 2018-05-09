72ef56a7a44edcfb25c7a43b4f7079dfd80c7cb8
Jools <julian@reisenberger.net>
2018-05-09 09:39:55 +0200

    wrap message in span for styling
    – with class cb-message

--- jquery.cookiebar.js
+++ jquery.cookiebar.js
@@ -140,9 +140,9 @@
             // Displays the cookie bar if arguments met
             if (options.forceShow || cookieValue == 'enabled' || cookieValue == '') {
                 if (options.append) {
-                    $(options.element).append('<div id="cookie-bar"' + fixed + zindex + '><p>' + message + acceptButton + declineButton + policyButton + '</p></div>');
+                    $(options.element).append('<div id="cookie-bar"' + fixed + zindex + '><p><span class="cb-message">' + message + '</span>' + acceptButton + declineButton + policyButton + '</p></div>');
                 } else {
-                    $(options.element).prepend('<div id="cookie-bar"' + fixed + zindex + '><p>' + message + acceptButton + declineButton + policyButton + '</p></div>');
+                    $(options.element).prepend('<div id="cookie-bar"' + fixed + zindex + '><p><span class="cb-message">' + message + '</span>' + acceptButton + declineButton + policyButton + '</p></div>');
                 }
             }
 
